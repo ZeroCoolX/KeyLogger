77856028e05e32c98f9b53cbffd13da0e9bb02d0
cnpatton <botanicality@gmail.com>
2019-06-14 19:53:53 -0500

    Hide the console window on startup by piggybacking windows.h Message functions

--- KeyLogger/main.cpp
+++ KeyLogger/main.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
+#include <windows.h>
 
-using namespace std;
-
-int main()
-{
-    cout << "Hello world!" << endl;
+int main(){
+    // Intercept any window and thread messages
+    // Translate it
+    // Resend it so that GetMessage() will grab it again
+    // This drops the UI Thread in an infinite loop so no window ever appears
+    MSG Msg;
+    while(GetMessage(&Msg, NULL, 0, 0)){
+        TranslateMessage(&Msg);
+        DispatchMessage(&Msg);
+    }
     return 0;
 }
